822b5ab99e2c9e86e79c2d0034f0eeab9eaac688
gellepogusunny-design <gellepogusunny@gmail.com>
2026-06-03 15:00:07 +0530

    sunny

---
+++ 24-05-2026/programs/P.32.2.py
@@ -0,0 +1,8 @@
+def sum_of_array(arr):
+    total = 0  
+    for element in arr:
+        total += element  
+    return total
+array = [1, 2, 3]
+result = sum_of_array(array)
+print("Sum of the array:", result)
