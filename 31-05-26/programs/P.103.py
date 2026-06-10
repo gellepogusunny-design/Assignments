fb364bdcf5a3bc9c108f243c4ba0e3c6f2589733
gellepogusunny-design <gellepogusunny@gmail.com>
2026-06-10 10:10:26 +0530

    sunny

---
+++ 31-05-26/programs/P.103.py
@@ -0,0 +1,6 @@
+writeyourcodehere = [1, 2, 3, 4, 5, 6]
+# Unpack the list into variables
+first, *middle, last = writeyourcodehere
+print(first)
+print(middle)
+print(last)
---
+++ 31-05-26/programs/P.107.py
@@ -0,0 +1,13 @@
+def reverse(value):
+    if isinstance(value, bool):
+       return not value
+    else:
+        return "boolean expected"
+
+print(reverse(True)) 
+
+print(reverse(False)) 
+
+print(reverse(0)) 
+
+print(reverse(None))
